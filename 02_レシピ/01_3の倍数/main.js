83658e11a0cb457978eb2dbb26b5b29807460d32
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-05 00:21:52 +0900

    [3の倍数]:発展3

--- 02_レシピ/01_3の倍数/main.js
+++ 02_レシピ/01_3の倍数/main.js
@@ -1,8 +1,52 @@
-const genkiFunction = function (number) {
-  for (let index = 1; index <= number; index++) {
-    if (index % 3 === 0) {
-      console.log(index + "!!!!!!!")
-    } else console.log(index)
-  }
+// 発展３ ３の倍数で元気になる関数
+// 解釈＝＞genkiFunction()無限に3の倍数をチェックする。
+let count = 0
+
+const genkiFunction = function () {
+  count += 1
+  if (count % 3 === 0) {
+    console.log(count + "!!!!!!!")
+  } else console.log(count)
 }
-genkiFunction(100)
+
+setInterval(genkiFunction, 1000)
+
+// 発展２文字列に３が入るときも元気になる
+// function genkiFunction(number) {
+//   var strNum
+//   for (var i = 1; i <= number; i++) {
+//     strNum = String(i)
+//     if (i % 3 === 0) {
+//       console.log(i + "!!!!!!")
+//     } else if (strNum.indexOf("3") != -1) {
+//       console.log(i + "!!!!!!")
+//     } else {
+//       console.log(i)
+//     }
+//   }
+// }
+// genkiFunction(100)
+
+// 発展１FizzBuzz関数
+// const fizzBuzz = function (number) {
+//   for (let index = 1; index <= number; index++) {
+//     if (index % 3 === 0 && index % 5 === 0) {
+//       console.log("FizzBuzz")
+//     } else if (index % 3 === 0) {
+//       console.log("Fizz")
+//     } else if (index % 5 === 0) {
+//       console.log("Buzz")
+//     } else console.log(index)
+//   }
+// }
+// fizzBuzz(15)
+
+// ３の倍数で元気になる関数
+// const genkiFunction = function (number) {
+//   for (let index = 1; index <= number; index++) {
+//     if (index % 3 === 0) {
+//       console.log(index + "!!!!!!!")
+//     } else console.log(index)
+//   }
+// }
+// genkiFunction(100)
