594886a4b28cb50cdc4c2d5967f1c637db404df5
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-14 12:15:58 +0900

    [Trello (保存機能なし)]を学習しました

---
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -0,0 +1,35 @@
+const inputElement = document.getElementById("input-todo")
+const container = document.getElementById("cards-container")
+const addButton = document.getElementById("add-button")
+
+addButton.onclick = function () {
+  // 入力欄のテキストを代入
+  const text = inputElement.value
+
+  // カード作成
+  const card = createCard(text)
+  container.append(card)
+
+  inputElement.value = ""
+}
+
+const createCard = function (text) {
+  const card = document.createElement("div")
+  card.className = "card"
+
+  // todo作成
+  const todo = document.createElement("div")
+  todo.className = "to-do"
+  todo.textContent = text
+  card.append(todo)
+
+  // 削除ボタン
+  const deleteButton = document.createElement("div")
+  deleteButton.className = "delete"
+  deleteButton.onclick = function () {
+    card.remove()
+  }
+
+  card.append(deleteButton)
+  return card
+}
