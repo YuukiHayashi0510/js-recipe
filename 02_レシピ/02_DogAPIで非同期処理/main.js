0556127b818a12b0159708c05f9fbf42dfce768b
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-16 21:55:55 +0900

    [DogAPIで非同期処理]発展１：Cat

--- 02_レシピ/02_DogAPIで非同期処理/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -1,14 +1,19 @@
-const imageElement = document.getElementById("dog-image")
+const dogImageElement = document.getElementById("dog-image")
+const catImageElement = document.getElementById("cat-image")
 
-// console.log("fetch before")
 // 指定したサーバーにデータを取りに行く
 fetch("https://dog.ceo/api/breeds/image/random")
   .then((res) => {
-    // console.log("one")
     return res.json() // 結果を json として読み込んで、次の then に渡す
   })
   .then((data) => {
-    // console.log("two")
-    imageElement.src = data.message // 画像を表示する
+    dogImageElement.src = data.message // 画像を表示する
   })
-// console.log("fetch after")
+
+fetch("https://api.thecatapi.com/v1/images/search")
+  .then((res) => {
+    return res.json()
+  })
+  .then((data) => {
+    catImageElement.src = data[0].url
+  })
