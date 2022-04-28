ca32f07012996a28a624f62ba4b49630901f2c7a
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-28 13:19:06 +0900

    [DogAPIで非同期処理]発展2:BreedsListまで学習しました。

--- 02_レシピ/02_DogAPIで非同期処理/main.js
+++ 02_レシピ/02_DogAPIで非同期処理/main.js
@@ -1,6 +1,18 @@
 const dogImageElement = document.getElementById("dog-image")
 const catImageElement = document.getElementById("cat-image")
 
+// 発展🌟１ワンちゃん切り替えボタン
+const turnButton = document.getElementById("turn-button")
+
+// 発展🌟２ 犬種指定切り替えボタン
+const fetchButton = document.getElementById("fetch-button")
+const breed = document.getElementById("breed")
+let url
+const firstUrl = "https://dog.ceo/api/breed/"
+const endUrl = "/images/random"
+const dogInput = document.getElementById("dog-input")
+const dogImageContainer = document.getElementById("dog-image-container")
+
 // 指定したサーバーにデータを取りに行く
 fetch("https://dog.ceo/api/breeds/image/random")
   .then((res) => {
@@ -17,3 +29,47 @@ fetch("https://api.thecatapi.com/v1/images/search")
   .then((data) => {
     catImageElement.src = data[0].url
   })
+
+// 発展🌟１ワンちゃん切り替えボタン
+turnButton.onclick = function () {
+  // 指定したサーバーにデータを取りに行く
+  fetch("https://dog.ceo/api/breeds/image/random")
+    .then((res) => {
+      return res.json() // 結果を json として読み込んで、次の then に渡す
+    })
+    .then((data) => {
+      dogImageElement.src = data.message // 画像を表示する
+    })
+}
+
+// 発展🌟２ 犬種指定切り替えボタン
+fetchButton.onclick = function () {
+  if (dogInput.value) {
+    dogImageContainer.textContent = ""
+    url = firstUrl + breed.value + endUrl + "/" + dogInput.value
+    fetch(url)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        for (let i = 0; i < Number(dogInput.value); i++) {
+          if (i === 0) {
+            dogImageElement.src = data.message[i]
+          } else {
+            const image = document.createElement("img")
+            image.src = data.message[i]
+            dogImageContainer.append(image)
+          }
+        }
+      })
+  } else {
+    url = firstUrl + breed.value + endUrl
+    fetch(url)
+      .then((res) => {
+        return res.json()
+      })
+      .then((data) => {
+        dogImageElement.src = data.message
+      })
+  }
+}
