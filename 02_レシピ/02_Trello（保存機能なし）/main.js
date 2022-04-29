865579092a7cb10be34b23b5a748e302127b0337
YuukiHayashi0510 <yuu.tennis.10.21.6@gmail.com>
2022-04-29 12:45:15 +0900

    [Trello (保存機能なし)]発展 ★★ - 複数のリスト:list-containerの追加まで

--- 02_レシピ/02_Trello（保存機能なし）/main.js
+++ 02_レシピ/02_Trello（保存機能なし）/main.js
@@ -2,6 +2,68 @@ const inputElement = document.getElementsByClassName("input-todo")
 const container = document.getElementsByClassName("cards-container")
 const addButton = document.getElementsByClassName("input-button")
 
+// list-container追加用の要素
+const mainContainer = document.getElementById("main-container")
+const listInputElement = document.getElementById("input-list")
+const listAddButton = document.getElementById("list-add-button")
+
+listAddButton.onclick = function () {
+  if (listInputElement.value !== "") {
+    const list = createList(listInputElement.value)
+
+    // main-container
+    mainContainer.append(list)
+    listInputElement.value = ""
+  }
+}
+
+// リストを作る関数
+const createList = function (text) {
+  // list-container
+  const listContainer = document.createElement("div")
+  listContainer.className = "list-container"
+
+  // list-header
+  const listHeader = document.createElement("div")
+  listHeader.className = "list-header"
+  console.dir(listInputElement)
+  listHeader.textContent = text
+
+  // cards-container
+  const cardsContainer = document.createElement("div")
+  cardsContainer.className = "cards-container"
+
+  // list-footer
+  const listFooter = document.createElement("div")
+  listFooter.className = "list-footer"
+
+  // input-container
+  const inputContainer = document.createElement("div")
+  inputContainer.className = "input-container"
+
+  // input-todo
+  const inputTodo = document.createElement("input")
+  inputTodo.className = "input-todo"
+
+  // input-button
+  const inputButton = document.createElement("div")
+  inputButton.className = "input-button"
+  inputButton.textContent = "追加"
+
+  // input-container append
+  inputContainer.append(inputTodo)
+  inputContainer.append(inputButton)
+  listFooter.append(inputContainer)
+
+  // list-container
+  listContainer.append(listHeader)
+  listContainer.append(cardsContainer)
+  listContainer.append(listFooter)
+
+  return listContainer
+}
+
+// addbutton
 for (let i = 0; i < inputElement.length; i++) {
   addButton[i].onclick = function (e) {
     // 入力欄のテキストを代入
@@ -13,17 +75,20 @@ for (let i = 0; i < inputElement.length; i++) {
 
     e.path[1].children[0].value = ""
   }
-  // 発展 ★ - エンターキー
-  window.onkeydown = function (e) {
+}
+
+// 発展 ★ - エンターキー
+window.onkeydown = function (e) {
+  for (let i = 0; i < inputElement.length; i++) {
     // keydown で押されたキーがエンターかつinputが空でなければ
     if (e.key === "Enter" && inputElement[i].value !== "") {
-      const text = e.path[1].children[0].value
+      const text = inputElement[i].value
 
       // カード作成
       const card = createCard(text)
       container[i].append(card)
 
-      e.path[1].children[0].value = ""
+      inputElement[i].value = ""
     }
   }
 }
